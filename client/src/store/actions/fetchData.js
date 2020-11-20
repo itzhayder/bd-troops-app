c39d509bc453ccbac2a78423fd5d09dbbc24b948
itzhayder <mdhayder.ali@northsouth.edu>
2020-11-21 03:18:59 +0600

    production ready

--- client/src/store/actions/fetchData.js
+++ client/src/store/actions/fetchData.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import * as actionTypes from './types/actionTypes';
-import { baseURL } from '../../config/config';
+// import { baseURL } from '../../config/config';
 
 const fetchData = () => async (dispatch) => {
   dispatch({ type: actionTypes.fetchStart });
 
   try {
-    const res = await axios.get(baseURL);
+    const res = await axios.get('/');
 
     dispatch({ type: actionTypes.fetchCompleted, payload: res.data });
   } catch (error) {
